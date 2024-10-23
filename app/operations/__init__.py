19276d82df3e42c6645451a20f74b5df74efec5c
Ria <vg435@njit.edu>
2024-10-23 13:13:02 -0400

    fixed pylint errors

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,13 +1,31 @@
-def Add(a, b):
+'''
+Can add, subtract, multiply, and divide. All inputs and outputs
+are typehinted to be floats.
+'''
+
+def add(a: float, b: float) -> float:
+    '''
+    Returns the sum of two floats.
+    '''
     return a + b
 
-def Subtract(a, b):
+def subtract(a: float, b: float) -> float:
+    '''
+    Returns the difference of two floats.
+    '''
     return a - b
 
-def Multiply(a, b):
+def multiply(a: float, b: float) -> float:
+    '''
+    Returns the product of two floats.
+    '''
     return a * b
 
-def Divide(a, b):
+def divide(a: float, b: float) -> float:
+    '''
+    Returns the quotient of two floats.
+    '''
     if b == 0:
+        # Sends an error message when someone tries to divide by zero.
         raise ValueError("Cannot divide by zero.")
     return a /b
