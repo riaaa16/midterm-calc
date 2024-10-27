70215de6a943993f72b6a81daa4de6fa91a869a9
Ria <vg435@njit.edu>
2024-10-27 14:50:04 -0400

    removed unnecesssary import

--- app/calculation/__init__.py
+++ app/calculation/__init__.py
@@ -1,5 +1,4 @@
 from app.operations import OperationTemplate
-from app.history import History
 
 class Calculation:
     def perform_operation(self, operation: OperationTemplate, operand1: float, operand2: float):
