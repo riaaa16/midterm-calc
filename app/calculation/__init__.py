6a997db7090e407ce6aaa078b2f23e38c2b5ef95
Ria <vg435@njit.edu>
2024-11-06 18:36:43 -0500

    added dataclass decorator and __repr__ and __str__

--- app/calculation/__init__.py
+++ app/calculation/__init__.py
@@ -1,6 +1,33 @@
+'''
+Uses OperationTemplate class provided by Operation_Factory to perform operations.
+'''
+from dataclasses import dataclass
 from app.operations import OperationTemplate
 
+@dataclass
 class Calculation:
-    def perform_operation(self, operation: OperationTemplate, operand1: float, operand2: float):
-        result = operation.calculate(operand1, operand2)  # Perform the calculation
-        return result
+    '''
+    Runs calculate method on operands and appends calculation to history.
+    Decorator automatically generates __init__ methods.
+    '''
+    operation: OperationTemplate  # operation to perform (add, subtract, etc.)
+    operand1: float  # first operand
+    operand2: float  # second operand
+
+    def __repr__(self) -> str:
+        '''String representation for debugging & logging'''
+        return (
+            f"Calculation({self.operation}, {self.operand1}, {self.operand2})"
+        )
+
+    def __str__(self) -> str:
+        '''String representation for users'''
+        result = self.operation.calculate(self.operand1, self.operand2)  # Perform the calculation
+        return (
+            f"{self.operand1} {self.operation} {self.operand2} = {result}"
+        )
+
+    def perform_operation(self):
+        '''Performs operation with provided operands'''
+        result = self.operation.calculate(self.operand1, self.operand2)  # Perform the calculation
+        return result  # return result
